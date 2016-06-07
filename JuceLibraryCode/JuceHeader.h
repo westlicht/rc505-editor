aebd0b422416cb111a4e60322894b865821d40f5
Simon Kallweit <simon@weirdsoft.ch>
2016-06-07 23:08:13 +0200

    project updates

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,7 +39,7 @@
 #if ! JUCE_DONT_DECLARE_PROJECTINFO
 namespace ProjectInfo
 {
-    const char* const  projectName    = "rc505-editor";
+    const char* const  projectName    = "RC-505";
     const char* const  versionString  = "0.0.1";
     const int          versionNumber  = 0x1;
 }
