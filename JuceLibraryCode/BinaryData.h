53f31b41ecdad97aebe9cb8fb4e8a67896630146
Simon Kallweit <simon@weirdsoft.ch>
2016-06-10 17:36:27 +0200

    added factory default data

--- JuceLibraryCode/BinaryData.h
+++ JuceLibraryCode/BinaryData.h
@@ -15,6 +15,12 @@ namespace BinaryData
     extern const char*   iconsmall_png;
     const int            iconsmall_pngSize = 10347;
 
+    extern const char*   MEMORY_RC0;
+    const int            MEMORY_RC0Size = 1233520;
+
+    extern const char*   SYSTEM_RC0;
+    const int            SYSTEM_RC0Size = 13020;
+
     extern const char*   COPYING;
     const int            COPYINGSize = 1475;
 
@@ -25,7 +31,7 @@ namespace BinaryData
     extern const char* namedResourceList[];
 
     // Number of elements in the namedResourceList array.
-    const int namedResourceListSize = 4;
+    const int namedResourceListSize = 6;
 
     // If you provide the name of one of the binary resource variables above, this function will
     // return the corresponding data and its size (or a null pointer if the name isn't found).
